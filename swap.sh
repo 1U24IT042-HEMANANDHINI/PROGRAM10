ecd1374f51ffee05d5a4ae8a51bb6f94c74ac93c
1U24IT042-HEMANANDHINI <nanthinisivakumar2006@gmail.com>
2026-09-11 12:15:47 +0530

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -2,10 +2,25 @@
 
 # =====================================
 # Swap Space Creation Script
-# Student Name:
-# Roll Number:
+# Student Name:Hema nandhini S
+# Roll Number:1U24IT042
 # =====================================
 
-# Write your commands below
+# Create a 1 GB swap file
+sudo fallocate -l 1G /swapfile
 
+# Set secure permissions
+sudo chmod 600 /swapfile
+
+# Format the file as swap
+sudo mkswap /swapfile
+
+# Enable swap
+sudo swapon /swapfile
+
+# Display swap information
+swapon --show
+
+# Display total swap space
+free -h
 
